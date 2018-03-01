847624ae3b436cc4c90907e495161af97c7d681d
Morgan Fudge <mfudge92@gmail.com>
2018-03-01 15:20:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(n) {
   if (n < 43) {
-    return 43 - n
-  } else if (n > 43) {
-    return n - 43
+    return (43 - n) + 1
   } else {
-    return 1
+    return (n - 43) + 1
   }
 }
